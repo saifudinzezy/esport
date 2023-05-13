2768b79196e7635e029cbfde59f15134747c5e37
saifudinzezy <saifudin.zezy7@gmail.com>
2023-05-13 12:33:08 +0700

    update

---
+++ app/src/main/java/com/example/rating/helper/Api.java
@@ -0,0 +1,10 @@
+package com.example.rating.helper;
+
+public class Api {
+    private static final String ROOT_URL = "https://esport.murah.shop/Api.php?apicall=";
+
+    public static final String URL_CREATE_HERO = ROOT_URL + "createhero";
+    public static final String URL_READ_HEROES = ROOT_URL + "getheroes";
+    public static final String URL_UPDATE_HERO = ROOT_URL + "updatehero";
+    public static final String URL_DELETE_HERO = ROOT_URL + "deletehero&id=";
+}
